06c591345b2f10fe1703307ea1d1faee2c2a9fd8
harry feng 13 inch mac <2307555175@qq.com>
2016-07-29 18:06:16 +0800

    playing js-xlsx

---
+++ testingjs-xlsx/index.js
@@ -0,0 +1,12 @@
+if(typeof require !== 'undefined') XLSX = require('xlsx');
+var workbook = XLSX.readFile('excelData.xlsx');
+var sheet_name_list = workbook.SheetNames;
+console.log(sheet_name_list);
+sheet_name_list.forEach(function(y) { /* iterate through sheets */
+    var worksheet = workbook.Sheets[y];
+    for (z in worksheet) {
+        /* all keys that do not begin with "!" correspond to cell addresses */
+        if(z[0] === '!') continue;
+        console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+    }
+});
