4d5633a8516cf98d10a01d1a013e60c869b4e040
harry feng 13 inch mac <2307555175@qq.com>
2016-08-16 21:34:30 +0800

    finish working on leancloud schema diff

--- leancloudGetSchema/getSchema.js
+++ leancloudGetSchema/getSchema.js
@@ -1,20 +1,20 @@
 var request = require('request');
 var md5 = require('md5');
 
-//var appId = "Kw627Ha1JP32IUOymijqLJe1-gzGzoHsz";
-//var appKey = "PlUKtpbqSLVs46xWPrMYXDNn";
-//var masterKey = "PdcRl6OLQ1jljTy5oTJkBenK";
-//var sign = md5(Date.now() + masterKey)
-//var signedTimeKey = sign + "," + Date.now() + "," + "master";
-//var signedKey = sign + "," + "master";
-//
-//var options = {
-//    url: ' https://api.leancloud.cn/1.1/schemas',
-//    headers: {
-//        "X-LC-Id": appId,
-//        "X-LC-Key": masterKey + "," + "master"
-//    }
-//};
+var diff = require('deep-diff').diff;
+
+var dbInfoA = {
+    appId:"Kw627Ha1JP32IUOymijqLJe1-gzGzoHsz",
+    appKey:"PlUKtpbqSLVs46xWPrMYXDNn",
+    masterKey:"PdcRl6OLQ1jljTy5oTJkBenK"
+};
+
+var dbInfoB = {
+    appId:"OAsq3BaUejlty6HR5y6LtoOA-gzGzoHsz",
+    appKey:"rY459mImPgBLwM4H78n3lbzI",
+    masterKey:"plvu0fCSPGdW3KcsmzUpff5a"
+};
+
 
 function makeRequestOption(appId,appKey,masterKey){
     var sign = md5(Date.now() + masterKey)
@@ -30,24 +30,14 @@ function makeRequestOption(appId,appKey,masterKey){
     return options;
 }
 
-//function callback(error, response, body) {
-//    if (!error && response.statusCode == 200) {
-//        var info = JSON.parse(body);
-//        console.log(info);
-//    } else {
-//        console.log(JSON.stringify(error));
-//        console.log(JSON.stringify(response));
-//    }
-//}
 
-request(makeRequestOption("Kw627Ha1JP32IUOymijqLJe1-gzGzoHsz","PlUKtpbqSLVs46xWPrMYXDNn","PdcRl6OLQ1jljTy5oTJkBenK"), function(error, response, body){
+request(makeRequestOption(dbInfoA.appId,dbInfoA.appKey,dbInfoA.masterKey), function(error, response, body){
     if (!error && response.statusCode == 200) {
         var firstSchema = JSON.parse(body);
-        console.log(firstSchema.xyqStudent);
-
-        request(makeRequestOption("OAsq3BaUejlty6HR5y6LtoOA-gzGzoHsz","rY459mImPgBLwM4H78n3lbzI","plvu0fCSPGdW3KcsmzUpff5a"), function(error, response, body){
+        request(makeRequestOption(dbInfoB.appId,dbInfoB.appKey,dbInfoB.masterKey), function(error, response, body){
             var secondSchema = JSON.parse(body);
-            console.log(secondSchema.xyqStudent);
+            var differences = diff(firstSchema, secondSchema);
+            console.log(differences);
         });
 
     } else {
