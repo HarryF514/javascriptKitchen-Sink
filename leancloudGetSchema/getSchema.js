abd3e634f6f3e04d0499d850211e8f43733856a5
harryfeng <2307555175@qq.com>
2016-08-17 11:39:33 +0800

    update lean schema compare

--- leancloudGetSchema/getSchema.js
+++ leancloudGetSchema/getSchema.js
@@ -3,20 +3,26 @@ var md5 = require('md5');
 
 var diff = require('deep-diff').diff;
 
-//
 var dbInfoA = {
+    appName:"海校齐友1.1",
     appId:"3SK2AEoHGRDYVJ2R89GPfe2L-gzGzoHsz",
     appKey:"MSku8mnEFRRIeg4DJqU6K0Ow",
     masterKey:"ExigflXqpOI9vLftJB56prnL"
 };
 
-// development
 var dbInfoB = {
+    appName:"海校齐友Development",
     appId:"OAsq3BaUejlty6HR5y6LtoOA-gzGzoHsz",
     appKey:"rY459mImPgBLwM4H78n3lbzI",
     masterKey:"plvu0fCSPGdW3KcsmzUpff5a"
 };
 
+var dbInfoC = {
+    appName:"海校齐友staging",
+    appId:"lCJUTtFR0fy55NddqvfQyXfw-gzGzoHsz",
+    appKey:"cLrB4KiuJw9PubqJi8uiopAv",
+    masterKey:"7A0J0DvQxjniGvd2rl9u2BGE"
+};
 
 function makeRequestOption(appId,appKey,masterKey){
     var sign = md5(Date.now() + masterKey)
@@ -34,11 +40,30 @@ function makeRequestOption(appId,appKey,masterKey){
 
 
 request(makeRequestOption(dbInfoA.appId,dbInfoA.appKey,dbInfoA.masterKey), function(error, response, body){
+
     if (!error && response.statusCode == 200) {
         var firstSchema = JSON.parse(body);
         request(makeRequestOption(dbInfoB.appId,dbInfoB.appKey,dbInfoB.masterKey), function(error, response, body){
             var secondSchema = JSON.parse(body);
             var differences = diff(firstSchema, secondSchema);
+            console.log("对比 " + dbInfoA.appName + " : " + dbInfoB.appName);
+            console.log(differences);
+        });
+
+    } else {
+        console.log(JSON.stringify(error));
+        console.log(JSON.stringify(response));
+    }
+});
+
+request(makeRequestOption(dbInfoC.appId,dbInfoC.appKey,dbInfoC.masterKey), function(error, response, body){
+
+    if (!error && response.statusCode == 200) {
+        var firstSchema = JSON.parse(body);
+        request(makeRequestOption(dbInfoB.appId,dbInfoB.appKey,dbInfoB.masterKey), function(error, response, body){
+            var secondSchema = JSON.parse(body);
+            var differences = diff(firstSchema, secondSchema);
+            console.log("对比 " + dbInfoC.appName + " : " + dbInfoB.appName);
             console.log(differences);
         });
 
