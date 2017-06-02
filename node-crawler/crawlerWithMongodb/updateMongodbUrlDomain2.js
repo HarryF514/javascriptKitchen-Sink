5c501c1c30800353f1df80d1f489eb0cd417972d
harry feng 13 inch mac <2307555175@qq.com>
2017-06-01 22:40:27 -0400

    update

---
+++ node-crawler/crawlerWithMongodb/updateMongodbUrlDomain2.js
@@ -0,0 +1,56 @@
+var Crawler = require("crawler");
+var _ = require("underscore");
+var ArticleParser = require('article-parser');
+var jsdom = require('jsdom');
+var url = require('url');
+var exec = require('child_process').exec;
+
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Grid = require('mongodb').Grid,
+    Code = require('mongodb').Code,
+    assert = require('assert');
+
+function log(s){
+    console.log(s);
+}
+
+function getDomain(url){
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
+MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
+    if(err) { return console.dir(err); }
+    var col = db.collection('Url');
+    var updateStart = {
+        find:function(){
+            col.findOne({urlDomain:{$exists:false}},function(err, docs) {
+                log(docs.url);
+                col.updateOne({url:docs.url}, {$set:{urlDomain:getDomain(docs.url)}});
+                updateStart.find();
+            });
+        }
+    };
+    updateStart.find();    
+
+})
