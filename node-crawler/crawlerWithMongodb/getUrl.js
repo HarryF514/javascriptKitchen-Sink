24bfbd7801b4db972463cca2b717f5f2dae9939f
harry feng 13 inch mac <2307555175@qq.com>
2017-06-03 15:07:22 -0400

    update

--- node-crawler/crawlerWithMongodb/getUrl.js
+++ node-crawler/crawlerWithMongodb/getUrl.js
@@ -49,75 +49,78 @@ MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
         col.updateMany({ isQueue: { $exists: false } }, { $set: { isQueue: false } }, o, function(err, r) {
             col.updateMany({ qualityPercentage: { $exists: false } }, { $set: { qualityPercentage: -1 } }, o, function(err, r) {
                 console.log("update result", r.result);
+                startGetUrl();
             });
         });
     });
 });
 
-MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
-    if (err) {
-        return console.dir(err);
-    }
-    var col = db.collection('Url');
-    var c = new Crawler({
-        maxConnections: 100,
-        timeout: 15000,
-        jQuery: jsdom,
-        // This will be called for each crawled page
-        callback: function(error, result, done) {
-            done();
-            try {
-                log(result.options.uri);
-
-                var $ = result.$;
-                $('a').each(function(index, a) {
-                    var toQueueUrl = $(a).prop('href').split('#')[0];
-                    var text = $(a).text().trim().replace(" ", "");
-                    if (escape(text).indexOf("%u") < 0) {
-                        //alert("没有包含中文");
-                        //console.log('no chinese', text);
-                    } else {
-                        //alert("包含中文");
-                        if (toQueueUrl.indexOf("http://") == 0) {
-                            col.find({ title: text }).toArray(function(err, docs) {
-                                if (docs.length === 0) {
-                                    console.log('title', text);
-                                    col.insertOne({ url: toQueueUrl, title: text, titleLength: text.length, urlDomain: getDomain(toQueueUrl), isQueue: false, isArticle: false, qualityPercentage: -1 });
-                                }
-                            })
-                        }
-                    }
-                })
-            } catch (e) {
-
-            }
+function startGetUrl() {
+    MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
+        if (err) {
+            return console.dir(err);
         }
-    })
+        var col = db.collection('Url');
+        var c = new Crawler({
+            maxConnections: 100,
+            timeout: 15000,
+            jQuery: jsdom,
+            // This will be called for each crawled page
+            callback: function(error, result, done) {
+                done();
+                try {
+                    log(result.options.uri);
 
-    setInterval(function() {
-        col.findOne({ isQueue: false }, function(err, docs) {
+                    var $ = result.$;
+                    $('a').each(function(index, a) {
+                        var toQueueUrl = $(a).prop('href').split('#')[0];
+                        var text = $(a).text().trim().replace(" ", "");
+                        if (escape(text).indexOf("%u") < 0) {
+                            //alert("没有包含中文");
+                            //console.log('no chinese', text);
+                        } else {
+                            //alert("包含中文");
+                            if (toQueueUrl.indexOf("http://") == 0) {
+                                col.find({ title: text }).toArray(function(err, docs) {
+                                    if (docs.length === 0) {
+                                        console.log('title', text);
+                                        col.insertOne({ url: toQueueUrl, title: text, titleLength: text.length, urlDomain: getDomain(toQueueUrl), isQueue: false, isArticle: false, qualityPercentage: -1 });
+                                    }
+                                })
+                            }
+                        }
+                    })
+                } catch (e) {
 
-            if (docs && docs.url) {
-                log("going to queue" + docs.url);
-                c.queue(docs.url);
-                col.updateMany({ url: docs.url }, { $set: { isQueue: true } });
+                }
             }
         })
-    }, 1000);
-    c.queue("http://bbs.51.ca/forum.php");
-    col.createIndex({ "url": 1 }, { unique: true });
-    col.createIndex({ title: "hashed" });
 
-});
+        setInterval(function() {
+            col.findOne({ isQueue: false }, function(err, docs) {
 
-setTimeout(function() {
-    exec("forever restart getUrl.js", function(error, stdout, stderr) {
-        if (error) {
-            console.log(error);
-            return;
-        }
-        if (stdout) {
-            console.log(stdout);
-        }
+                if (docs && docs.url) {
+                    log("going to queue" + docs.url);
+                    c.queue(docs.url);
+                    col.updateMany({ url: docs.url }, { $set: { isQueue: true } });
+                }
+            })
+        }, 1000);
+        c.queue("http://bbs.51.ca/forum.php");
+        col.createIndex({ "url": 1 }, { unique: true });
+        col.createIndex({ title: "hashed" });
+
     });
-}, 10 * 60000);
+
+    setTimeout(function() {
+        exec("forever restart getUrl.js", function(error, stdout, stderr) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            if (stdout) {
+                console.log(stdout);
+            }
+        });
+    }, 10 * 60000);
+}
