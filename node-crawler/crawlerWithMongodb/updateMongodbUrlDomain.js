157334368f38d9653ab3f9b3a9c1ad268e0bf7c7
harry feng 13 inch mac <2307555175@qq.com>
2017-06-03 22:03:29 -0400

    update

--- node-crawler/crawlerWithMongodb/updateMongodbUrlDomain.js
+++ node-crawler/crawlerWithMongodb/updateMongodbUrlDomain.js
@@ -16,18 +16,17 @@ var Db = require('mongodb').Db,
     Code = require('mongodb').Code,
     assert = require('assert');
 
-function log(s){
+function log(s) {
     console.log(s);
 }
 
-function getDomain(url){
+function getDomain(url) {
     var hostname;
     //find & remove protocol (http, ftp, etc.) and get hostname
 
     if (url.indexOf("://") > -1) {
         hostname = url.split('/')[2];
-    }
-    else {
+    } else {
         hostname = url.split('/')[0];
     }
 
@@ -39,23 +38,27 @@ function getDomain(url){
     return hostname;
 }
 
-MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
-    if(err) { return console.dir(err); }
+MongoClient.connect("mongodb://localhost:27017/articledb", {
+    keepAlive: 30000,
+    connectTimeoutMS: 30000,
+}, function(err, db) {
+    if (err) {
+        return console.dir(err); }
     var col = db.collection('Url');
     var updateStart = {
-        find:function(){
-            col.find({urlDomain:{$exists:false}}).limit(100).toArray(function(err, docs) {
+        find: function() {
+            col.find({ urlDomain: { $exists: false } }).limit(1000).toArray(function(err, docs) {
                 log(docs);
-                _.each(docs,function(element,index,list){
-                    col.updateOne({url:element.url}, {$set:{urlDomain:getDomain(element.url)}});
+                _.each(docs, function(element, index, list) {
+                    col.updateOne({ url: element.url }, { $set: { urlDomain: getDomain(element.url) } });
                 });
                 //col.updateOne({url:docs.url}, {$set:{urlDomain:getDomain(docs.url)}});
-                setTimeout(function(){
-                     updateStart.find();
-                 },100);
+                setTimeout(function() {
+                    updateStart.find();
+                }, 100);
             });
         }
     };
-    updateStart.find();    
+    updateStart.find();
 
 })
