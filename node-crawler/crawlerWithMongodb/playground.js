d53981af1510fcb627906e12cafcecdd14facdb2
harry feng 13 inch mac <2307555175@qq.com>
2017-06-25 16:12:27 -0400

    update article id

--- node-crawler/crawlerWithMongodb/playground.js
+++ node-crawler/crawlerWithMongodb/playground.js
@@ -42,7 +42,10 @@ var initdb = function() {
 
         function startInsert() {
             var ran_number = Math.random();
-            col.insert({ url: "http://bbs.51.ca/member.php?mod" + ran_number, title: 't' + ran_number }, function() {
+            col.insert({
+                url: "http://bbs.51.ca/member.php?mod" + ran_number,
+                title: 't' + ran_number
+            }, function() {
                 console.log("random number", ran_number);
                 startInsert();
             })
@@ -57,10 +60,28 @@ var updateIsArticleFalse = function() {
             return console.dir(err);
         }
         var col = db.collection('Url');
-        var o = { w: 1 };
+        var o = {
+            w: 1
+        };
         o.multi = true
-        col.updateMany({ isArticle: { $exists: false } }, { $set: { isArticle: false } }, o, function(err, r) {
-            col.updateMany({ isQueue: { $exists: false } }, { $set: { isQueue: false } }, o, function(err, r) {
+        col.updateMany({
+            isArticle: {
+                $exists: false
+            }
+        }, {
+            $set: {
+                isArticle: false
+            }
+        }, o, function(err, r) {
+            col.updateMany({
+                isQueue: {
+                    $exists: false
+                }
+            }, {
+                $set: {
+                    isQueue: false
+                }
+            }, o, function(err, r) {
                 console.log("update result", r.result);
             });
         });
@@ -75,7 +96,11 @@ var queueGetArticle = function() {
         var col = db.collection('Url');
         var Articlecol = db.collection('ArticleParser');
         var callbackCounter = 0;
-        col.find({ isArticle: false }).sort({ qualityPercentage: -1 }).limit(100).toArray(function(e, results) {
+        col.find({
+            isArticle: false
+        }).sort({
+            qualityPercentage: -1
+        }).limit(100).toArray(function(e, results) {
             console.log("queueGetArticle find", results);
 
             function startGetArticle() {
@@ -84,15 +109,17 @@ var queueGetArticle = function() {
                     log("article length " + article.content.length);
                     if (article.content.length > 2000) {
                         //log("article length " + article.title);
-                        Articlecol.find({ title: article.title }).toArray(function(err, docs) {
-                        	if(err){
-                        		return console.log("article err",err);
-                        	}
+                        Articlecol.find({
+                            title: article.title
+                        }).toArray(function(err, docs) {
+                            if (err) {
+                                return console.log("article err", err);
+                            }
                             console.log("docs.length", docs.length);
                             if (docs.length === 0) {
                                 console.log("Articlecol.insertOne", article.title);
-                                article.id = 
-                                Articlecol.insertOne(article);
+                                article.id =
+                                    Articlecol.insertOne(article);
                             }
 
                         })
@@ -103,7 +130,15 @@ var queueGetArticle = function() {
                     log("ArticleParser.extract error " + err.toString());
                     //callback();
                 }).finally(function() {
-                    col.updateMany({ url: results[0].url }, { $set: { isArticle: true } }, { w: 1 }, function(err, r) {
+                    col.updateMany({
+                        url: results[0].url
+                    }, {
+                        $set: {
+                            isArticle: true
+                        }
+                    }, {
+                        w: 1
+                    }, function(err, r) {
                         console.log("update", r.result);
                     });
                     callbackCounter++;
@@ -123,21 +158,65 @@ var queueGetArticle = function() {
     });
 }
 
-var updateArtcelIdField = function () {
+var updateArtcelIdField = function() {
     var objectId = new ObjectID().toString();
     console.log(objectId);
-     MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
+    MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
         if (err) {
             return console.dir(err);
         }
         var Articlecol = db.collection('ArticleParser');
-        Articlecol.findOneAndUpdate({id:{$exists:false}},{$set: {id:objectId}},function (err,doc) {
+        Articlecol.findOneAndUpdate({
+            id: {
+                $exists: false
+            }
+        }, {
+            $set: {
+                id: objectId
+            }
+        }, function(err, doc) {
             console.log(doc);
             db.close();
             updateArtcelIdField();
         })
     });
 }
-updateArtcelIdField();
+
+var removeEnglishTitleArticle = function(counter) {
+    MongoClient.connect("mongodb://localhost:27017/articledb", function(err, db) {
+        if (err) {
+            return console.dir(err);
+        }
+        var Articlecol = db.collection('ArticleParser');
+        Articlecol.find().limit(1000).skip(1000*counter).toArray(function(err, docs) {
+            console.log(docs.length);
+            if(docs.length === 0){
+                console.log('done');
+                return;
+            }
+            _.each(docs, function(element, index, list) {
+                if (escape(element.title).indexOf("%u") < 0) {
+                    Articlecol.removeOne({
+                        _id: element._id
+                    }, {
+                        w: 1
+                    }, function(err, r) {
+                        console.log(r.result);
+                        db.close();
+                    });
+                }
+                console.log(index);
+                if(index >= docs.length - 1){
+                    db.close();
+                    console.log('counter',counter);
+                    counter++
+                    removeEnglishTitleArticle(counter);
+                }
+            });
+        });
+    });
+}
+
+removeEnglishTitleArticle(0);
 //updateIsArticleFalse();
 //initdb();
