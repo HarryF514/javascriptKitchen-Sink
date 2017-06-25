a21c4add365fc951759e42093a5f802ed5578ce1
harry feng 13 inch mac <2307555175@qq.com>
2017-06-25 13:47:21 -0400

    update

--- node-crawler/crawlerWithMongodb/connectMongoRemote.js
+++ node-crawler/crawlerWithMongodb/connectMongoRemote.js
@@ -1,15 +1,21 @@
 var MongoClient = require('mongodb').MongoClient
-var assert = require('assert')
-var express = require('express')
-var app = express()
 
-var db = null
-MongoClient.connect('mongodb://139.59.252.180:27017/test', function(err,database) {
-	if(err){
-	console.log(err);
-	return
+var remote_db = null
+MongoClient.connect('mongodb://139.59.252.180:27017/test', function(err, database) {
+	if (err) {
+		console.log(err);
+		return
+	}
+	console.log(database);
+	remote_db = database
+	//remote_insert_many(remote_db);
+	
+});
+
+function remote_insert_many(db,obj_array) {
+	var col = db.collection('insert_many');
+	var col = db.collection('insert_many');
+	col.insertMany(obj_array, function(err, r) {
+		db.close();
+	});
 }
-console.log(database);
-    assert.equal(err,null)
-    db = database
-})
