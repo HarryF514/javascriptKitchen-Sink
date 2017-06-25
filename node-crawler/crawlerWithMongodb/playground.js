c19bc30550dd8fb5e2bc8177059653196ab95fc7
harry feng 13 inch mac <2307555175@qq.com>
2017-06-25 15:37:55 -0400

    update article id

--- node-crawler/crawlerWithMongodb/playground.js
+++ node-crawler/crawlerWithMongodb/playground.js
@@ -133,6 +133,7 @@ var updateArtcelIdField = function () {
         var Articlecol = db.collection('ArticleParser');
         Articlecol.findOneAndUpdate({id:{$exists:false}},{$set: {id:objectId}},function (err,doc) {
             console.log(doc);
+            db.close();
             updateArtcelIdField();
         })
     });
