17a037f15ffbc789d9b4776fc2446abc56a4df35
harryfeng <harry@knowroaming.com>
2017-07-17 10:26:59 -0400

    update

--- crawlerWithMongodb/connectMongoRemote.js
+++ crawlerWithMongodb/connectMongoRemote.js
@@ -9,15 +9,17 @@ MongoClient.connect('mongodb://139.59.252.180:27017/articledb', function(err, db
 	remote_db = db;
 	var Articlecol = db.collection('ArticleParser');
 	var UrlCol = db.collection('Url');
-	// Articlecol.count().then(function (docs) {
-	// 	console.log('Articlecol count', docs);
-	// });
-	// UrlCol.count().then(function (docs) {
-	// 	console.log('UrlCol count', docs);
-	// });
+	
+	Articlecol.count().then(function(docs) {
+	    console.log('Articlecol.count() count', docs);
+	});
+	
+	UrlCol.count().then(function(docs) {
+	    console.log('UrlCol.count() count', docs);
+	});
 
 	UrlCol.count({id:{$exists:false}}).then(function (docs) {
-		console.log('UrlCol count', docs);
+		console.log('UrlCol.count({id:{$exists:false}}) count', docs);
 		db.close();
 	});
 });
