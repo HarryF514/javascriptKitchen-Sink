cf69b4fdf01b747ffab29858e22bbbd450a9e83b
harryfeng <2307555175@qq.com>
2017-08-07 12:49:51 -0400

    add

---
+++ crawlerWithMongodb/getArticleByApi.js
@@ -0,0 +1,31 @@
+var request = require('request');
+
+var ArticleParser = require('article-parser');
+
+ArticleParser.configure({
+    timeout: 15 * 1000
+});
+(function go() {
+    request('http://9i9icenter.com/db/getUrl', function(error, response, body) {
+        if (error) {
+            return console.log('error', error);
+        };
+        ArticleParser.extract(JSON.parse(body).url).then(function(article) {
+            //console.log('article.content.length', article.content.length);
+            if (article.content.length > 2000) {
+                request.post({
+                    url: 'http://9i9icenter.com/db/addArticle',
+                    form: article
+                }, function(error, response, body) {
+                	go();
+                    if (error) {
+                        return console.log('error', error);
+                    }
+                    //console.log('body', body);
+                });
+            }else{
+            	go();
+            }
+        });
+    })
+})();
