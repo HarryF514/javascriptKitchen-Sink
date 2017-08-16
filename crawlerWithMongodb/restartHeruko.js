ced06ba8060650192b60709fd021b112a7ce246f
harry feng 13 inch mac <2307555175@qq.com>
2017-08-15 21:01:06 -0400

    add restart heroku script

---
+++ crawlerWithMongodb/restartHeruko.js
@@ -0,0 +1,54 @@
+var exec = require('child_process').exec;
+
+setInterval(function() {
+	exec('curl -n -X DELETE https://api.heroku.com/apps/getarticle/dynos   -H "Content-Type: application/json"   -H "Accept: application/vnd.heroku+json; version=3"', function(error, stdout, stderr) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		if (stdout) {
+			console.log(stdout);
+		}
+	});
+
+	exec('curl -n -X DELETE https://api.heroku.com/apps/getar/dynos   -H "Content-Type: application/json"   -H "Accept: application/vnd.heroku+json; version=3"', function(error, stdout, stderr) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		if (stdout) {
+			console.log(stdout);
+		}
+	});
+
+	exec('curl -n -X DELETE https://api.heroku.com/apps/getarticle1/dynos   -H "Content-Type: application/json"   -H "Accept: application/vnd.heroku+json; version=3"', function(error, stdout, stderr) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		if (stdout) {
+			console.log(stdout);
+		}
+	});
+
+	exec('curl -n -X DELETE https://api.heroku.com/apps/getarticle2/dynos   -H "Content-Type: application/json"   -H "Accept: application/vnd.heroku+json; version=3"', function(error, stdout, stderr) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		if (stdout) {
+			console.log(stdout);
+		}
+	});
+
+	exec('curl -n -X DELETE https://api.heroku.com/apps/getarticle3/dynos   -H "Content-Type: application/json"   -H "Accept: application/vnd.heroku+json; version=3"', function(error, stdout, stderr) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		if (stdout) {
+			console.log(stdout);
+		}
+	});
+
+}, 10*60000);
