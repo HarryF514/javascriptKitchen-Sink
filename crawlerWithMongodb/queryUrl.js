70432a4615a57dd67edae20420a9bb132046c8a9
Harry Feng <harry@knowroaming.com>
2017-11-09 17:40:04 -0500

    update

--- crawlerWithMongodb/queryUrl.js
+++ crawlerWithMongodb/queryUrl.js
@@ -2,13 +2,31 @@
  * @Author: Harry Feng
  * @Date:   2017-11-09 16:54:14
  * @Last Modified by:   Harry Feng
- * @Last Modified time: 2017-11-09 17:33:20
+ * @Last Modified time: 2017-11-09 17:39:47
  */
 
 var request = require('request'),
 	cheerio = require('cheerio');
 var jsdom = require('jsdom');
 const queryString = require('query-string');
+
+function getDomain(url) {
+	var hostname;
+	//find & remove protocol (http, ftp, etc.) and get hostname
+
+	if (url.indexOf("://") > -1) {
+		hostname = url.split('/')[2];
+	} else {
+		hostname = url.split('/')[0];
+	}
+
+	//find & remove port number
+	hostname = hostname.split(':')[0];
+	//find & remove "?"
+	hostname = hostname.split('?')[0];
+
+	return hostname;
+}
 
 function parse(url) {
 	request(url, function(error, response, body) {
@@ -17,35 +35,33 @@ function parse(url) {
 		var obj = JSON.parse(body);
 		request(obj, function(error, response, body) {
 			var $ = cheerio.load(body);
-			$('a').prop
-			console.log('jjj', $('a').prop)
 			$('a').each(function(index, a) {
-				var toQueueUrl = $(this).prop('href').split('#')[0];
+
 				var text = $(a).text().trim().replace(" ", "");
 				if (!/.*[\u4e00-\u9fa5]+.*$/.test(text)) {
 					//alert("没有包含中文");
 					//console.log('no chinese', text);
 				} else {
-					//alert("包含中文");
-					if (toQueueUrl.indexOf("http://") == 0) {
-						var objectId = new ObjectID().toString();
-						col.insertOne({
-							url: toQueueUrl,
-							id: objectId,
-							title: text,
-							titleLength: text.length,
-							urlDomain: getDomain(toQueueUrl),
-							isQueue: false,
-							isArticle: false,
-							qualityPercentage: -1
-						}, function(err, r) {
-							if (err) {
-								console.log('err ', err);
-							} else {
-								console.log('inserted ', text)
-							}
-						});
+					if ($(this).prop('href')) {
+						var toQueueUrl = $(this).prop('href').split('#')[0];
+						//alert("包含中文");
+						if (toQueueUrl.indexOf("http://") == 0) {
+							//var objectId = new ObjectID().toString();
+							var obj = {
+								url: toQueueUrl,
+								title: text,
+								titleLength: text.length,
+								urlDomain: getDomain(toQueueUrl),
+								isQueue: false,
+								isArticle: false,
+								qualityPercentage: -1
+							};
+							const stringified = queryString.stringify(obj);
+							console.log('stringified', stringified);
+							
+						}
 					}
+
 				}
 			});
 		})
