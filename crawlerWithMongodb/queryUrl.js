a2086b44add5cd25855f5609d9a06d2592840fe6
harryfeng <2307555175@qq.com>
2017-11-18 17:13:35 -0500

    update

--- crawlerWithMongodb/queryUrl.js
+++ crawlerWithMongodb/queryUrl.js
@@ -9,6 +9,8 @@ var request = require('request'),
     cheerio = require('cheerio');
 var jsdom = require('jsdom');
 const queryString = require('query-string');
+var exec = require('child_process').exec;
+
 
 function getDomain(url) {
     var hostname;
@@ -30,31 +32,31 @@ function getDomain(url) {
 
 function parse(url) {
     request(url, function(error, response, body) {
-        if(body === undefined){
-        	parse('http://localhost:3000/');
-        	return;
+        if (body === undefined) {
+            parse('http://localhost:3000/');
+            return;
         }
-        try{
+        try {
             var obj = JSON.parse(body);
-        }catch(e){
+        } catch (e) {
             console.log('body error: ', e);
             parse('http://localhost:3000/');
             return
         }
-        
+
         console.log('obj.url', obj.url);
         request(obj.url, function(error, response, body) {
-        	try {
-        		// statements
-        		 var $ = cheerio.load(body);
-        	} catch(e) {
-        		// statements
-        		console.log(e);
-        		parse('http://localhost:3000/');
-        		return;
+            try {
+                // statements
+                var $ = cheerio.load(body);
+            } catch (e) {
+                // statements
+                console.log(e);
+                parse('http://localhost:3000/');
+                return;
 
-        	}
-           
+            }
+
             var requestBody = [];
             $('a').each(function(index, a) {
 
@@ -85,9 +87,11 @@ function parse(url) {
 
             request.post({
                 url: 'http://localhost:3000/save',
-                form: {data: JSON.stringify(requestBody)}
+                form: {
+                    data: JSON.stringify(requestBody)
+                }
             }, function(error, response, body) {
-            	parse('http://localhost:3000/');
+                parse('http://localhost:3000/');
                 if (error) {
                     return console.log('error', error)
                 } else {
@@ -99,10 +103,15 @@ function parse(url) {
 }
 
 parse('http://localhost:3000/');
-// parse('http://localhost:3000/');
-// parse('http://localhost:3000/');
-// parse('http://localhost:3000/');
-// parse('http://localhost:3000/');
-// parse('http://localhost:3000/');
-// parse('http://localhost:3000/');
-// parse('http://localhost:3000/');
+
+setTimeout(function() {
+    exec("forever restart queryUrl.js", function(error, stdout, stderr) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        if (stdout) {
+            console.log(stdout);
+        }
+    });
+}, 60000);
